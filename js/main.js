e221e630b68851c9e5918595c758754992d13558
Kevin O'Toole <kmotoole78@Kevins-MacBook-Pro.local>
2013-03-11 21:43:42 -0400

    deleted the option group from the additem.html page, and created javascript to populate the goup on the main.js page

--- js/main.js
+++ js/main.js
@@ -14,11 +14,33 @@ window.addEventListener("DOMContentLoaded", function(){
 	}
 
 
-	// Variable defaults
+	// Create select field element and populate with options
+	function recipeCategory(){
+		var formTag = document.getElementsByTagName("form"); //formTag is an array of all the form tags
+		var selectLi =$("selectType");
+		var makeSelect = document.createElement("select");
+		makeSelect.setAttribute("id", "types");
+		for(var i=0, j=recipeType.length; i<j; i++){
+			var makeOption = document.createElement("option");
+			var optText = recipeType[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);
+	}
 
+	// Variable defaults
+	var recipeType = [" --Choose A Type Of Recipe-- ", "Breakfast", "Lunch", "Dinner", "Appetizer", "Dessert", "Drink"]
+	recipeCategory();
 
 
 	//Set Link & Submit Click Events
+	/*var displayRecipes = $("display");
+	displayRecipes.addEventListener("click", getRecipes);
+	var clearRecipes =$("clear");
+	clearRecipes.addEventListener("click", deleteLocalRecipes);
+	var saveNewRecipe = $("saveRecipe");
+	saveNewRecipe.addEventListener("click", storeNewRecipe);*/
 
-
-};
+});
